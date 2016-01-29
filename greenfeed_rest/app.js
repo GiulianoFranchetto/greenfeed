f088e1768a0e27f4b1ecdb7142a06b888996d262
GiulianoFranchetto <giuliano.franchetto@intellinium.com>
2016-01-29 11:46:12 +0100

    Basic Auth for client auth on REST server

--- greenfeed_rest/app.js
+++ greenfeed_rest/app.js
@@ -4,6 +4,7 @@
 var express    = require('express');
 var app        = express();                 // define our app using express
 var bodyParser = require('body-parser');
+var authorization = require('basic-auth');
 
 // configure app to use bodyParser()
 // this will let us get the data from a POST
@@ -26,35 +27,36 @@ var UserSchema = new mongoose.Schema({
     firstname: String,
     email: String,
     password: String,
-    Created_at: { type: Date, default: Date.now }
+    created_at: { type: Date, default: Date.now }
 });
 
 var AbriSchema = new mongoose.Schema({
     v_bat: Number,
     p_bat: Number,
-    mppt_o: Number
+    mppt_o: Number,
+    time: Number
 });
 
-var User = mongoose.model('user', UserSchema);
+var User = mongoose.model('user', UserSchema, 'user');
 var Abri = mongoose.model('abri', AbriSchema, 'abri');
 
 /* Get all entries for "Abri" */
 app.get('/abri', function(req, res){
-    Abri.find().limit(100).exec(function(err, values){
+    Abri.find().sort({$natural: -1}).limit(30).exec(function(err, values){
         if (err) {
             return res.sendStatus(500);
         }
-        console.log(values);
         res.status(200).send(values);
     });
 });
 
 app.get('/user', function(req, res){
-    User.find({email: req.get.email, password: req.get.password }, function(err, values){
+    var user = authorization(req);
+    User.find({'email': user.name, 'password': user.pass }, function(err, values){
         if(err){
             return res.sendStatus(500);
         }
-        if(values.length === 0) {
+        if(!values.length) {
             return res.sendStatus(403);
         }
         return res.sendStatus(200);
