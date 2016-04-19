daf070d51624a16235cd0f0c61c2344a0b8c3db1
GiulianoFranchetto <giuliano.franchetto@intellinium.com>
2016-04-19 14:29:38 +0200

    removing a warning

--- greenfeed_rpi/includes/UsbComm.h
+++ greenfeed_rpi/includes/UsbComm.h
@@ -4,13 +4,13 @@
     \version 1.0
     \date 31/03/2015
 
-    \brief Ce fichier d�clare la classe qui va g�rer la communication entre la Raspberry et l'Arduino
-    Dans la version 1.0 : cette classe ne g�re que le lien Arduino -> RPi concernant l'envoi en LoRa.
-    La partie CAN sera faite quand le shield aura �t� re�u
+    \brief Ce fichier déclare la classe qui va gérer la communication entre la Raspberry et l'Arduino
+    Dans la version 1.0 : cette classe ne gère que le lien Arduino -> RPi concernant l'envoi en LoRa.
+    La partie CAN sera faite quand le shield aura été reéu
 
-    Cette classe s'appuie sur les ent�tes syst�me de Linux. En effetm une communication USB n'est en fait que l'�criture dans
-    un fichier, car tout n'es tque fichier en Linux. La communication a un baudrate de 9600 fix�. Si, pour une raison quelconque,
-    ce baudrate doit �tre chang�, IL FAUT AUSSI PENSER A LE CHANGER DANS LE CODE DE L'ARDUINO.
+    Cette classe s'appuie sur les entétes systéme de Linux. En effetm une communication USB n'est en fait que l'écriture dans
+    un fichier, car tout n'est que fichier en Linux. La communication a un baudrate de 9600 fixé. Si, pour une raison quelconque,
+    ce baudrate doit étre changé, IL FAUT AUSSI PENSER A LE CHANGER DANS LE CODE DE L'ARDUINO.
 */
 
 #ifndef USBCOMM_H
@@ -31,8 +31,8 @@
 #include <unistd.h>
 
 #define NOTHING void* rien
-#define usb_r   int /*!< D�clare un nouveau type qui repr�sentera le r�sultat d'une op�ration en USB*/
-#define usb_interface int /*!< D�clare un nouveau type qui repr�sentera le fichier dans lequel sera �crit les donn�es � envoyer en USB*/
+#define usb_r   ssize_t /*!< Déclare un nouveau type qui représentera le résultat d'une opération en USB*/
+#define usb_interface int /*!< Déclare un nouveau type qui représentera le fichier dans lequel sera écrit les données é envoyer en USB*/
 #define USB_BAUDRATE    B9600 /*!< Le baudrate du l'USB*/
 #define BUFF_SIZE   200
 
@@ -44,21 +44,21 @@ using namespace std;
 /**
     \class UsbComm
     \brief Cette classe regroupe toutes les fonctions haut niveau afin d'ouvrir une communication USB entre deux appareils et envoyer des
-    donn�es
+    données
 */
 class UsbComm {
  public:
   /*!
-     \brief Constructeur par d�faut
+     \brief Constructeur par défaut
 
-     Constructeur par d�faut de la classe UsbComm
+     Constructeur par défaut de la classe UsbComm
    */
   UsbComm ();
 
   /*!
      \brief Ouvre la communication en USB
 
-     Cette m�thode initialise la communication en USB
+     Cette méthode initialise la communication en USB
 
      \param const string phy_interface : l'interface physique de l'USB (classiquement /dev/ttyACM0)
      \return 0 si tout va bien,
@@ -67,12 +67,12 @@ class UsbComm {
   usb_r ouvrirCommunication (const string);
 
   /*!
-     \brief Envoie une donn�e en USB
+     \brief Envoie une donnée en USB
 
-     Cette m�thode envoie une donn�e en USB. Si le message n'est pas termin� par "\n", alors ce caract�re
+     Cette méthode envoie une donnée en USB. Si le message n'est pas terminé par "\n", alors ce caractére
      est rajouter automatiquement.
 
-     \param char* message : le message � envoyer
+     \param char* message : le message é envoyer
      \param bool changeInB64 : if set to true, the char* in parameter will be converted to Base64 string
      \return 0 si tout va bien,
      -1 sinon
@@ -80,10 +80,10 @@ class UsbComm {
   usb_r envoyerMessage (char *, bool);
 
   /*!
-      \brief Re�oit une donn�e en USB
+      \brief Reéoit une donnée en USB
 
       \param char* message : buffer dans lequel le message sera mis
-      \return 0 si rien n'a �t� lu,
+      \return 0 si rien n'a été lu,
       -1 si erreur
    */
   usb_r recevoirMessage (char *);
@@ -91,12 +91,12 @@ class UsbComm {
   /*!
      \brief Destructeur de la classe UsbComm
 
-     Cette m�thode d�truit l'objet UsbComm, ferme la connexion USB et lib�re la m�moire.
+     Cette méthode détruit l'objet UsbComm, ferme la connexion USB et libére la mémoire.
    */
   virtual ~UsbComm ();
  protected:
  private:
-  usb_interface interface; /*!< Le fichier repr�sentant la connection USB*/
+  usb_interface interface; /*!< Le fichier représentant la connection USB*/
 };
 
 #endif // USBCOMM_H
